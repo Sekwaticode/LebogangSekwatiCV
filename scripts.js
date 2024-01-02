19d7b577781353ddebfd7cfdaa26028d6036910b
Sekwaticode <lsekwati2@gmail.com>
2024-01-02 22:43:00 +0200

    animations added

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick = () => { 
-    menuIcon.classList.toggle("bx-x");
-    navbar.classList.toggle("active");
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
 
-let section = document.querySelectorAll("section");
+let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-  section.forEach((sec) => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
@@ -32,3 +32,27 @@ window.onscroll = () => {
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
+
+//scroll reveal
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .skills-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+const types = new Typed(".multiple-text", {
+  strings: ["Web Developer", "Web Designer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  backDelay: 1000,
+  loop: true,
+});
