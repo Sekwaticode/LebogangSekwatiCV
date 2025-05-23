cf9762512ecbaa9e814f1096086fe6cafcce5e3e
Sekwaticode <lsekwati2@gmail.com>
2025-05-23 13:22:35 +0200

    half done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@ import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { ProductShowcase } from "@/sections/ProductShowcase";
-import Projects  from "@/sections/Projects";
+import Projects from "@/sections/Projects";
 import { Testimonials } from "@/sections/Testimonials";
 import { CallToAction } from "@/sections/CallToAction";
 import { Footer } from "@/sections/Footer";
 import FormToWhatsApp from "@/sections/FormToWhatsApp"; // Adjust path based on file location
-
 
 export default function Home() {
   return (
@@ -18,7 +17,7 @@ export default function Home() {
       <ProductShowcase />
       <Projects />
       <Testimonials />
-      <FormToWhatsApp/>
+      <FormToWhatsApp />
       <CallToAction />
       <Footer />
     </>
